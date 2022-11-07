64c941b72007592b88372dbae209c49efc36aa42
bossmonkey98 <arunambregol007@gmail.com>
2022-11-07 13:17:05 +0530

    [feat]: screens defined

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home, Parking, Payment } from './Screens/'
+import { Provider } from 'react-redux';
+import { store } from './Store/Store'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+export type ScreenParamTypes = {
+  Home: undefined;
+  Parking: undefined;
+  Payment: undefined;
+}
 
 export default function App() {
+  const Stack = createNativeStackNavigator<ScreenParamTypes>()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Provider store={store}>
+          <View style={styles.container}>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={Home} options={{ title: 'Home' }} />
+            <Stack.Screen name='Parking' component={Parking} options={{ title: "Parking Space" }} />
+            <Stack.Screen name='Payment' component={Payment} options={{title:"Payment"}}/>
+            </Stack.Navigator>
+            <StatusBar style="auto" />
+          </View>
+        </Provider>
+    </NavigationContainer>
   );
 }
 
@@ -14,7 +35,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
